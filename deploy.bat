09c38b1800aba16349c2716705b1d95eddb8b6b6
thiroyoshi <thiroyoshi@gmail.com>
2023-04-02 20:52:07 +0900

    Update deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,2 +1,2 @@
 cd src/video-converter
-gcloud functions deploy video-converter --gen2 --runtime=go116 --region=asia-northeast1 --entry-point=VideoConverter --trigger-http --allow-unauthenticated
+gcloud functions deploy video-converter --gen2 --runtime=go120 --region=asia-northeast1 --entry-point=VideoConverter --trigger-http --allow-unauthenticated
